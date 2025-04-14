e6aca792f30e7871bce01797697c5d0d0463e0b0
Kevin Ammerman <k.ammerman.git@pm.me>
2025-04-14 18:20:30 +0200

    Implement database fetching for phonebook entries using Mongoose

--- index.js
+++ index.js
@@ -1,28 +1,8 @@
+require('dotenv').config();
+const Person = require('./models/persons');
 const express = require('express');
 const morgan = require('morgan');
 const app = express();
-let persons = [
-  {
-    id: '1',
-    name: 'Arto Hellas',
-    number: '040-123456',
-  },
-  {
-    id: '2',
-    name: 'Ada Lovelace',
-    number: '39-44-5323523',
-  },
-  {
-    id: '3',
-    name: 'Dan Abramov',
-    number: '12-43-234345',
-  },
-  {
-    id: '4',
-    name: 'Mary Poppendieck',
-    number: '39-23-6423122',
-  },
-];
 
 app.use(
   morgan((tokens, req, res) => {
@@ -48,7 +28,9 @@ const checkIfContactExists = (name) => {
 };
 
 app.get('/api/persons', (request, response) => {
-  response.json(persons);
+  Person.find({}).then((persons) => {
+    response.json(persons);
+  });
 });
 
 app.get('/api/persons/:id', (request, response) => {
@@ -92,7 +74,7 @@ app.get('/info', (request, response) => {
   response.send(info);
 });
 
-const PORT = process.env.PORT || 3001;
+const PORT = process.env.PORT;
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
