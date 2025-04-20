099a548d231a51565c17e00da4df2ac2400d7deb
Kevin Ammerman <k.ammerman.git@pm.me>
2025-04-20 14:13:59 +0200

    Add custom validation for phone numbers in database
    Implement custom validator in Mongoose schema to ensure phone numbers
    have at least 8 digits, are split by a hyphen, with the first part
    having 2 or 3 digits and the second part consisting of numbers. Return
    appropriate error status and message for invalid numbers. Reference
    Exercise 3.20.

--- models/persons.js
+++ models/persons.js
@@ -20,7 +20,16 @@ const personSchema = new mongoose.Schema({
     minLength: 3,
     required: true,
   },
-  number: Number,
+  number: {
+    type: String,
+    required: [true, 'User phone number required'],
+    validate: {
+      validator: function (v) {
+        return /^(?:\d{2}-\d{6,}|\d{3}-\d{5,})$/.test(v);
+      },
+      message: (props) => `${props.value} is not a valid number`,
+    },
+  },
 });
 
 personSchema.set('toJSON', {
